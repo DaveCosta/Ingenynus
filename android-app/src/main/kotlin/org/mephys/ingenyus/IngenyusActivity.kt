569bcada9ebef27e40a4c1d3f266e10b3672589a
David Da Costa <david.emanuel.da.costa@gmail.com>
2019-05-15 23:41:43 +0200

    New upgrade

--- android-app/src/main/kotlin/org/mephys/ingenyus/IngenyusActivity.kt
+++ android-app/src/main/kotlin/org/mephys/ingenyus/IngenyusActivity.kt
@@ -2,11 +2,36 @@ package org.mephys.ingenyus
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
+import java.net.URL
+import java.util.concurrent.Executors
+
 
 class IngenyusActivity() : Activity() {
-    protected override fun onCreate(savedInstanceState : Bundle?) {
+    protected override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var result :String? = null
+
+       Executors.newSingleThreadExecutor().execute {
+           result = URL("https://httpbin.org/get").readText()
+        }
+
+        Thread(Runnable {
+            // (1)
+            //This is in the onCreate method
+            val lView = LinearLayout(this)
+
+            val myText = TextView(this)
+            myText.setText(result)
+
+            lView.addView(myText)
+
+            setContentView(lView)
+        }).start()                       // (3)
     }
+
 }
 
